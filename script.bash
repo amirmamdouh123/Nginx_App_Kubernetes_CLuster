a6d5edf4b0f02d5247142281bc3ab0a49c8f393f
Amir <amir.mam.helmy@gmail.com>
2025-02-01 19:56:31 +0200

    add kubernetes manifest & dockerfile & script to run

--- script.bash
+++ script.bash
@@ -1,12 +1,17 @@
 
 #docker login -uamirmamdouh123 -p{}
 
-docker build --tag amirmamdouh123/amir_say_hello_nginx . 
+echo "NEW IMAGE BUILDING..."
+docker build --tag amirmamdouh123/amir_say_hello_nginx .
+echo "BUILDING IMAGE FINISHED."
 
+echo "PUSH IMAGE TO DOCKER REGISTRY..."
 docker push amirmamdouh123/amir_say_hello_nginx
+echo "IMAGE IS PUSHED."
 
+echo "APPLING UPDATES TO KUBERNETES DEPLOYMENT..."
 kubectl apply -f nginx-deployment.yml
-
+echo "FINISHED SUCCESSFULLY."
 
 
 
